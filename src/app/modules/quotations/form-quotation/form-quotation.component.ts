1bea31d32235353fbfc70291007622ca51ad0bb9
islam-ali <ai4625511@gmail.com>
2023-08-20 03:29:37 +0300

    merge

--- src/app/modules/quotations/form-quotation/form-quotation.component.ts
+++ src/app/modules/quotations/form-quotation/form-quotation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { QuotationsService } from '../quotations.service';
 
 @Component({
   selector: 'app-form-quotation',
@@ -8,116 +9,180 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormQuotationComponent implements OnInit {
   AddClientFileForm!: FormGroup;
-  ListOfItems:any[] = [
+  ListOfItems: any[] = [
     {
-      name:'الجرانيت',
-      id:16
+      name: 'الجرانيت',
+      value: 'garanet',
+      itemTypeId:4,
+      id: 16
     },
     {
-      name:'المجلى',
-      id:12
+      name: 'المجلى',
+      value: 'magla',
+      itemTypeId:4,
+      id: 12
     },
     {
-      name:'البانيل',
-      id:15
+      name: 'البانيل',
+      value: 'panel',
+      itemTypeId:4,
+      id: 15
     },
     {
-      name:'حفرة المجلى',
-      id:13
+      name: 'حفرة المجلى',
+      value: 'maglaHole',
+      itemTypeId:4,
+      id: 13
     },
     {
-      name:'تصفيح الجدران من التوب',
-      id:227
+      name: 'تصفيح الجدران من التوب',
+      value: 'platingTopWall',
+      itemTypeId:4,
+      id: 227
     },
     {
-      name:'سترب خارجي',
-      id:83
+      name: 'سترب خارجي',
+      value: 'outerStrop',
+      itemTypeId:4,
+      id: 83
     },
     {
-      name:'نوع الايادي',
-      id:9
+      name: 'نوع الايادي',
+      value: 'handType',
+      itemTypeId:4,
+      id: 9
     },
     {
-      name:'الشفاط',
-      id:23
+      name: 'الشفاط',
+      value: 'shafat',
+      itemTypeId:4,
+      id: 23
     },
     {
-      name:'تسميك التوب',
-      id:276
+      name: 'تسميك التوب',
+      value: 'thickeningTop',
+      itemTypeId:4,
+      id: 276
     },
     {
-      name:'البطارية',
-      id:14
+      name: 'البطارية',
+      value: 'batery',
+      itemTypeId:4,
+      id: 14
     },
     {
-      name:'الكورنيش',
-      id:178
+      name: 'الكورنيش',
+      value: 'corniche',
+      itemTypeId:4,
+      id: 178
     },
     {
-      name:'توصيلات',
-      id:274
+      name: 'توصيلات صحية',
+      value: 'healthLinking',
+      itemTypeId:4,
+      id: 274
     },
     {
-      name:'الانارة',
-      id:275
+      name: 'الانارة',
+      value: 'lighting',
+      itemTypeId:4,
+      id: 275
+    },
+    // {
+    //   name: 'تصفيح خشب ',
+    //   value:'',
+    //   id: 96
+    // },
+    // {
+    //   name: 'متطلبات العميل',
+    //   value:'',
+    //   id: 0
+    // },
+
+    // ======================
+    {
+      name: 'الوحدات',
+      value: 'unites',
+      id: 1,
+      itemTypeId: 1
     },
     {
-      name:'xxxxxxxx',
-      id:0
-    },
-    {
-      name:'متطلبات العميل',
-      id:0
-    },
-    {
-      name:'الوحدات',
-      id:2
-    },
-    {
-      name:'الاضافات',
-      id:2
+      name: 'الاضافات',
+      accessories: {
+        name: '',
+        value: 'accessories',
+        id: 3,
+        itemTypeId: 3
+      },
+      value: 'accessories',
+      id: 3,
+      itemTypeId: 3
     },
   ]
+  loadPriceOffer: any;
+  UnitsItemsbyCategory: any;
   constructor(
-    private _FormBuilder: FormBuilder
+    private _FormBuilder: FormBuilder,
+    private _QuotationsService: QuotationsService
   ) {
     this.AddClientFileForm = this.initClientFileForm();
-    this.ListOfItems.forEach((ele:any , index:number) => {
-      this.addItemsFormArray()
+    this.ListOfItems.forEach((ele: any, index: number) => {
+      this.addItemsFormArray();
       this.itemsFormArray.controls[index].patchValue({
-        itemId:ele.id
+        categoryId: ele.id,
+        itemTypeId: ele.itemTypeId
       })
     })
   }
-  initClientFileForm() :FormGroup{
+  initClientFileForm(): FormGroup {
     return this._FormBuilder.group({
-      clientId: [0 , [Validators.required]],
-      deviceNotes: ['' , [Validators.required]],
-      fileTypeId: [0 , [Validators.required]],
-      additionaldiscount:  [0 , [Validators.required]],
+      clientId: [0, [Validators.required]],
+      deviceNotes: ['', [Validators.required]],
+      fileTypeId: [0, [Validators.required]],
+      additionaldiscount: [0, [Validators.required]],
       items: this._FormBuilder.array([])
     })
   }
-  ClientFileFormGroup() :FormGroup{
+  ClientFileFormGroup(): FormGroup {
     return this._FormBuilder.group({
-      itemId: [0 , [Validators.required]],
-      itemCount: [0 , [Validators.required]],
-      itemTypeId: [0 , [Validators.required]],
+      itemId: [0, [Validators.required]],
+      itemCount: [0, [Validators.required]],
+      itemTypeId: [0, [Validators.required]],
       itemPrice: [0],
       notes: [0],
       categoryId: [0]
     })
   }
-  get  itemsFormArray(){
+  get itemsFormArray() {
     return this.AddClientFileForm.controls["items"] as FormArray;
   }
+
   addItemsFormArray() {
     this.itemsFormArray.push(this.ClientFileFormGroup());
   }
   deleteItemsFormArray(index: number) {
     this.itemsFormArray.removeAt(index);
   }
+  LoadPriceOffer() {
+    this._QuotationsService.LoadPriceOffer().subscribe({
+      next: (res: any) => {
+        this.loadPriceOffer = res.data
+      }
+    })
+  }
+  GetUnitsItemsbyCategory() {
+    this._QuotationsService.GetUnitsItemsbyCategory().subscribe({
+      next: (res: any) => {
+        this.UnitsItemsbyCategory = res.data
+      }
+    })
+  }
   ngOnInit(): void {
-      
+    this.LoadPriceOffer()
+    this.GetUnitsItemsbyCategory()
+  }
+  onSubmit() {
+    console.log(this.AddClientFileForm.value);
+
   }
 }
--- src/app/modules/quotations/quotations.service.ts
+++ src/app/modules/quotations/quotations.service.ts
@@ -8,14 +8,20 @@ export class QuotationsService {
   domain: string = 'http://194.163.132.242:5000/api/';
 
   constructor(private _HttpClient: HttpClient) { }
-  
+
   GetAllClientFiles(): Observable<any> {
     return this._HttpClient.get(`${this.domain}ClientFile/GetAllClientFiles`)
   }
   AddClientFile(): Observable<any> {
     return this._HttpClient.get(`${this.domain}ClientFile/AddClientFile`)
   }
-  GetStatusCategoryById(id:number): Observable<any> {
+  GetStatusCategoryById(id: number): Observable<any> {
     return this._HttpClient.get(`${this.domain}StatusCategory/GetStatusCategoryById/${id}`)
+  }
+  LoadPriceOffer(): Observable<any> {
+    return this._HttpClient.get(`${this.domain}ClientFile/LoadPriceOffer`)
+  }
+  GetUnitsItemsbyCategory(): Observable<any> {
+    return this._HttpClient.get(`${this.domain}StatusCategory/GetUnitsItemsbyCategory`)
   }
 }
