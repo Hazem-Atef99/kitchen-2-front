f2ffe123a24347fdfe10e70753c434303508ffc5
hazem <hazem.foda000@gmail.com>
2023-12-06 21:39:19 +0200

    edit

--- src/app/modules/reception-report/form-reception-report/form-reception-report.component.ts
+++ src/app/modules/reception-report/form-reception-report/form-reception-report.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Clients, DataClients } from '../../clients/modal/clients';
 import { ClientsService } from '../../clients/clients.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-form-reception-report',
@@ -31,8 +32,64 @@ export class FormReceptionReportComponent {
       id: 6
     },
   ]
+  devices :any=[
+    {
+      name:'Fridge 60',
+      id:1
+    },
+    {
+      name:'Fridge 90',
+      id:1
+    },
+    {
+      name:'Freezer 60',
+      id:1
+    },
+    {
+      name:'Freezer 90',
+      id:1
+    },
+    {
+      name:'Oven 60',
+      id:1
+    },
+    {
+      name:'Oven 90',
+      id:1
+    },
+    {
+      name:'Hop',
+      id:1
+    },
+    {
+      name:'Warming Drawer',
+      id:1
+    },
+    {
+      name:'Microwave',
+      id:1
+    },
+    {
+      name:'Cofee Maker',
+      id:1
+    },
+    {
+      name:'Cooler',
+      id:1
+    },
+    {
+      name:'Dish Washer',
+      id:1
+    },
+    {
+      name:'Washing Machine',
+      id:1
+    },
+
+  ]
   constructor(private _FormBuilder: FormBuilder,
-              private _ClientsService: ClientsService,) {
+              private _ClientsService: ClientsService,
+              private toastr: ToastrService,) {
     this.AddClientFileForm = this.initClientFileForm();
   }
   ngOnInit(): void {
--- src/app/modules/reception-report/reception-report.component.ts
+++ src/app/modules/reception-report/reception-report.component.ts
@@ -1,5 +1,8 @@
+import { ToastrService } from 'ngx-toastr';
+import { QuotationsService } from '../quotations/quotations.service';
 import { ReceptionReportService } from './reception-report.service';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-reception-report',
@@ -12,9 +15,22 @@ export class ReceptionReportComponent implements OnInit {
     fileTypeId: null,
   }
   allReceptionReports: any;
-  constructor(private recptionReportService:ReceptionReportService) { }
+  clientFileId: number = 0;
+  statusId: number = 0;
+  allClientFileAttachment: any[] = [];
+  statusCategoryById: any;
+  viewImg: any[] = [];
+  uploadedImg: any[] = [];
+  Domain: any = environment.apiUrl;
+  LoadFinalStatusList: any[] = [];
+  Note: String = '';
+  AllFinalStatusClientFile: any[] = [];
+  constructor(private recptionReportService:ReceptionReportService,
+              private _QuotationsService:QuotationsService,
+              private toastr: ToastrService,) { }
   ngOnInit(): void {
     this.getReceptionRports()
+    this.GetStatusCategoryById()
   }
   filter(event: any) {
     console.log(event.value);
@@ -24,6 +40,88 @@ export class ReceptionReportComponent implements OnInit {
   getReceptionRports(){
     this.recptionReportService.getReceptionReport().subscribe((res:any)=>{
       this.allReceptionReports=res.data
+    })
+  }
+  GetAllClientFileAttachment() {
+    let query = {
+      clientFileId: this.clientFileId,
+      statusId: this.statusId,
+    }
+    this._QuotationsService.GetAllClientFileAttachment(query).subscribe({
+      next: (res: any) => {
+        this.allClientFileAttachment = res.data
+      }
+    })
+  }
+  onImageSelected(event: any): void {
+    this.viewImg = []
+    this.uploadedImg = []
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      this.uploadedImg.push(event.target.files.item(0));
+      reader.onload = (event: any) => {
+        this.viewImg.push(event.target.result);
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+  AddClientFileAttachment() {
+    let value: any = {};
+    value['clientFileId'] = this.clientFileId;
+    value['attachmentPath'] = this.uploadedImg[0];
+    value['statusId'] = this.statusId;
+    this._QuotationsService.AddClientFileAttachment(value).subscribe({
+      next: (res: any) => {
+        this.toastr.success(`${res.message}`);
+        this.viewImg = []
+        this.uploadedImg = []
+        this.GetAllClientFileAttachment()
+       // this.GetShortClientFiles();
+      }, error: (err: any) => {
+        this.toastr.error(`${err.message}`);
+      }
+    })
+  }
+  GetStatusCategoryById() {
+    this._QuotationsService.GetStatusCategoryById(100).subscribe({
+      next: (res: any) => {
+        this.statusCategoryById = res.data
+        console.log()
+        this.statusId = res.data.statuses[0].statusId
+      }
+    })
+  }
+  GetLoadFinalStatusList() {
+    console.log(this.query.PageType)
+    this._QuotationsService.LoadFinalStatusList(this.query.PageType).subscribe({
+      next: (res: any) => {
+        this.LoadFinalStatusList = res.data.statuses
+      }
+    })
+  }
+  AddFinalStatusList() {
+    let value: any = {};
+    value['clientFileId'] = this.clientFileId;
+    value['finalStatusId'] = this.statusId;
+    value['notes'] = this.Note;
+    this._QuotationsService.AddFinalStatusListApi(value).subscribe({
+      next: (res: any) => {
+        this.toastr.success(`${res.message}`);
+        this.viewImg = []
+        this.uploadedImg = []
+        this.GetAllFinalStatusClientFile()
+       // this.GetShortClientFiles();
+      }, error: (err: any) => {
+        this.toastr.error(`${err.message}`);
+      }
+    })
+  }
+  GetAllFinalStatusClientFile() {
+    this._QuotationsService.AllFinalStatusClientFile(this.clientFileId).subscribe({
+      next: (res: any) => {
+        console.log(res)
+        this.AllFinalStatusClientFile  = res.data
+      }
     })
   }
 }
